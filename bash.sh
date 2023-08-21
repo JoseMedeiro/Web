78f46420854b6a0240c30c415857449af72b2536
JoseMedeiro <56878235+JoseMedeiro@users.noreply.github.com>
2023-08-21 12:06:37 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# a
-
-find ./ -depth -name "*.html" -exec sed -i 's|index.html|../Web|g' about.html
-find ./ -depth -name "*.html" -exec sed -i 's|\.html| |g' about.html
+sed -i 's|index.html|../Web|g' *.html
+sed -i 's|\.html| |g' *.html
